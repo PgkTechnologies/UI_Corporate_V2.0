b170b8bb44034b0cb4e5dbcea45081f5d09e9dbf
srinupgk <109269306+srinupgk@users.noreply.github.com>
2023-07-03 15:42:28 +0530

    new updates and router fix

--- src/index.js
+++ src/index.js
@@ -1,66 +1,56 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import { toast } from 'react-toastify';
-import { applyMiddleware, createStore } from 'redux';
+import React from "react";
+import { applyMiddleware, createStore } from "redux";
 import ReactDOM from "react-dom/client";
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import 'react-toastify/dist/ReactToastify.css';
-import AllReducers from './Store/Reducers';
-import { webportalAppRootSaga } from './Store/Sagas';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import history from './@history';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { AuthProvider } from './utils/Auth';
-
-// toast({
-//   autoClose: 4000,
-//   draggable: false,
-//   position: toast.POSITION.TOP_RIGHT,
-//   hideProgressBar: true, 
-//   pauseOnFocusLoss: false,
-//   });
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import "react-toastify/dist/ReactToastify.css";
+import AllReducers from "./Store/Reducers";
+import { webportalAppRootSaga } from "./Store/Sagas";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./utils/Auth";
 
 // Middleware: Redux Saga
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(AllReducers, composeWithDevTools(applyMiddleware(sagaMiddleware, thunk)));
+const store = createStore(
+  AllReducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware, thunk))
+);
 // const store = createStore(AllReducers, applyMiddleware(thunk));
 
 sagaMiddleware.run(webportalAppRootSaga);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
-      <Router
-        history={history} basename="/"
-      >
+    <Provider store={store}>
+      <BrowserRouter>
         <AuthProvider>
-        <App />
-        <ToastContainer
-          position="top-right"
-          autoClose={3500}
-          hideProgressBar
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          pauseOnHover
-          theme="dark"
-        />
+          <App />
+          <ToastContainer
+            position="top-right"
+            autoClose={3500}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            pauseOnHover
+            theme="dark"
+          />
         </AuthProvider>
-      </Router>
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
   // document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
